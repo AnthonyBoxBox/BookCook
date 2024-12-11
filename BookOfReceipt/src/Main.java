546c53f40fa27740e9aba3c851062be35bc16836
AnthonyBoxBox <149335368+AnthonyBoxBox@users.noreply.github.com>
2024-12-11 07:20:27 +0100

    Update Main.java

--- BookOfReceipt/src/Main.java
+++ BookOfReceipt/src/Main.java
@@ -1,45 +1,43 @@
-                                                
-import java.sql.Connection;                     
-import java.sql.DriverManager;                  
-import java.sql.PreparedStatement;              
-import java.sql.ResultSet;                      
-import java.sql.SQLException;                   
-import java.sql.Statement;                      
-public class Main {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+public class Main{
 	public static void main(String[] args){
-		String pass = "";                                                                                                
-        
-		String url = "jdbc:mysql://localhost:3306/cookbook";                                                             
-		String user = "root";                                                                                            
-		                                                                                                                 
-		try (Connection connection = DriverManager.getConnection(url, user, pass);                                       
-		     PreparedStatement statement = connection.prepareStatement(                                                  
-		             		"SELECT p.nazwa, s.nazwa_skladnika, ps.ilosc, s.jednostka_miary " +                          
-		                     "FROM przepisy p " +                                                                        
-		                     "JOIN przepisy_skladniki ps ON p.id_przepisu = ps.id_przepisu " +                           
-		                     "JOIN skladniki s ON ps.id_skladnika = s.id_skladnika " +                                   
-		                     "WHERE p.id_przepisu = ?")) {                                                               
-		                                                                                                                 
-		                                                                                                                 
-		    statement.setInt(1, 2);                                                                                      
-		                                                                                                                 
-		                                                                                                                 
-		    ResultSet resultSet = statement.executeQuery();                                                              
-		                                                                                                                 
-		                                                                                                                 
-		    while (resultSet.next()) {                                                                                   
-		        String nazwaPrzepisu = resultSet.getString("nazwa");                                                     
-		        String nazwaSkladnika = resultSet.getString("nazwa_skladnika");                                          
-		        int ilosc = resultSet.getInt("ilosc");                                                                   
-		        String jednostkaMiary = resultSet.getString("jednostka_miary");                                          
-		                                                                                                                 
-		        System.out.println("Przepis: " + nazwaPrzepisu);                                                         
-		        System.out.println("Składnik: " + nazwaSkladnika + " (" + ilosc + " " + jednostkaMiary + ")");           
-		    }                                                                                                            
-		                                                                                                                 
-		} catch (SQLException e) {                                                                                       
-		    e.printStackTrace();                                                                                         
-		}                                                                                                                
-	        
-	 }
-}
+		Connection conn = Database.connect();
+		
+		try {
+			PreparedStatement statement = conn.prepareStatement("SELECT * FROM przepisy");
+			
+			ResultSet resultSet = statement.executeQuery();
+			ArrayList<Przepis> przepisyy = new ArrayList<Przepis>();
+
+			while (resultSet.next()) {    
+				Boolean vege = resultSet.getBoolean("vege");
+				Przepis przepiss;
+				if(vege) {
+					przepiss = new PrzepisVege(resultSet.getInt("ilosc_skladnikow"), resultSet.getString("instrukcje"), resultSet.getString("nazwa"));
+				} else {
+					przepiss = new Przepis(resultSet.getInt("ilosc_skladnikow"), resultSet.getString("instrukcje"), resultSet.getString("nazwa"));
+				}
+				przepisyy.add(przepiss);
+				
+			}                             
+			for(Przepis p: przepisyy) {
+				p.wypiszPrzepis();
+			}
+		
+		
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		          	
+		             	
+		          
+
+
+}}
